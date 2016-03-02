dc26269a704090564a52ba514271939bd72d4e1b
FredLoney <loneyf@ohsu.edu>
2016-03-01 16:26:16 -0800

    Export cornerstone if a module loader is enabled.

--- src/header.js
+++ src/header.js
@@ -1,6 +1,23 @@
-if(typeof cornerstone === 'undefined'){
-    cornerstone = {
+// Create a temporary variable to hold the cornerstone
+// module. If cornerstone is already in the global
+// namespace, then modify that object. Otherwise,
+// make a new object.
+if(typeof this.cornerstone === 'undefined') {
+    var cornerstone = {
         internal : {},
         rendering: {}
     };
+} else {
+  var cornerstone = this.cornerstone;
+}
+
+// If a module loader is enabled, then export this cornerstone module.
+// Otherwise, add this module to the global namespace if necessary.
+if(typeof exports !== 'undefined') {
+  if(typeof module !== 'undefined' && module.exports) {
+    exports = module.exports = cornerstone;
+  }
+  exports.cornerstone = cornerstone;
+} else if(typeof this.cornerstone === 'undefined') {
+  this.cornerstone = cornerstone;
 }
