f9cbedf18deff34a55533d8bb9967b44736eb524
FredLoney <loneyf@ohsu.edu>
2016-03-24 12:16:40 -0700

    Add cornerstone event properties.

--- src/eventEngineFactory.js
+++ src/eventEngineFactory.js
@@ -3,45 +3,75 @@
     "use strict";
     
     var createEventEngine;
-
-    if(typeof $ === 'undefined') {
-        // Delegate to the eventEmitter module.
-        createEventEngine = function(element) {
-            var ee = new EventEmitter();
-            // Return an EventEmitter adapter that aliases two
-            // EventEmitter functions.
-            return {
-                on: function(evt, fn) {
-                    // An EventEmitter handler function has a single data
-                    // argument, e.g. handler(data). By contrast, jQuery
-                    // has two arguments (event, data) The jQuery event
-                    // argument is seldom used in practice. If there is
-                    // more than one handler argument, then complain. 
-                    if (fn.length > 1) {
-                        throw new Error("The built-in non-jQuery Cornerstone" +
-                                        " event handler function must accept" +
-                                        " at most a single data argument.");
-                    }
-                    return ee.on.apply(ee, arguments); },
-                trigger: function() { return ee.emit.apply(ee, arguments); }
-            };
-        };
-    }
-    else {
-        // Delegate to jQuery.
-        createEventEngine = function(element) {
-            return $(element);
-        };
-    }
     
-    // module/private exports
-    cornerstone.createEventEngine = createEventEngine;
-    // Make the Cornerstone event engine.
-    var ee = createEventEngine(cornerstone);
+    // The built-in EventEmitter adapter.
+    var builtinFactory = function(element) {
+        var ee = new EventEmitter();
+        // Return an EventEmitter adapter that aliases two
+        // EventEmitter functions.
+        return {
+            on: function(evt, fn) {
+                // An EventEmitter handler function has a single data
+                // argument, e.g. handler(data). By contrast, jQuery
+                // has two arguments (event, data) The jQuery event
+                // argument is seldom used in practice. If there is
+                // more than one handler argument, then complain.
+                if (fn.length > 1) {
+                    throw new Error("The built-in non-jQuery Cornerstone" +
+                                    " event handler function must accept" +
+                                    " at most a single data argument.");
+                }
+                return ee.on.apply(ee, arguments); },
+            trigger: function() { return ee.emit.apply(ee, arguments); }
+        };
+    };
+    
+    // The jQuery adapter.
+    var jQueryFactory = function(element) {
+        return $(element);
+    };
+    
+    Object.defineProperties(cornerstone, {
+        // Creates the event engine factory on demand, if necessary.
+        eventEngineFactory: {
+            get: function() {
+                if(typeof this._eeFactory === 'undefined') {
+                    if(typeof $ === 'undefined') {
+                        this._eeFactory = builtinFactory;
+                    }
+                    else {
+                        this._eeFactory = jQueryFactory;
+                    }
+                }
+                return this._eeFactory;
+            },
+            set: function(fn) { this._eeFactory = fn; }
+        },
+        // Creates the cornerstone event engine on demand.
+        eventEngine: {
+            get: function() {
+                if(typeof this._eventEngine === 'undefined') {
+                    this._eventEngine = this.createEventEngine(this);
+                }
+                return this._eventEngine;
+            }
+        }
+    });
+
+    // Delegate event engine creation to the factory.
+    cornerstone.createEventEngine = function(element) {
+        var factory = cornerstone.eventEngineFactory;
+        return factory(element);
+    };
+
     // Delegate the Cornerstone event emitter and handler to the
     // event engine.
-    cornerstone.on = function() { return ee.on.apply(ee, arguments); };
+    cornerstone.on = function() {
+        var ee = cornerstone.eventEngine;
+        return cornerstone.eventEngine.on.apply(ee, arguments);
+    };
     cornerstone.trigger = function() {
-        return ee.trigger.apply(ee, arguments);
+        var ee = cornerstone.eventEngine;
+        return cornerstone.eventEngine.trigger.apply(ee, arguments);
     };
 }(cornerstone));
